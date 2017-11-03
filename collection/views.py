c05befa3712b2240d17ca8184842f44cb90e2883
vincent <vinniestratz@gmail.com>
2017-11-03 14:59:14 -0400

    Second Commit

--- collection/views.py
+++ collection/views.py
@@ -3,5 +3,17 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    # defining the variable
+    number = 6
+    # quotes for strings... obviously
+    thing = "Thing name"
+    # passing the variable to the view
+    return render(request, 'index.html', {
+        'number': number,
+        'thing': thing,
+    })
+
+
+def about(request):
     # new view
-    return render(request, 'index.html')
+    return render(request, 'about.html')
--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import TemplateView
 from collection import views
 
 urlpatterns = [
     url(r'^$', views.index, name='home'),
+    url(r'^about-us/$', views.about, name='about'),
+    url(r'^contact/$',
+        TemplateView.as_view(template_name='contact.html'),
+        name='contact'),
     url(r'^admin/', admin.site.urls),
 ]
